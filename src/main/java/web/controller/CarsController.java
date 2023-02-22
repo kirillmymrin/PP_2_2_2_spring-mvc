e47e82086bda411b397911478e79eb448d649e68
kirillmymrin <113543878+kirillmymrin@users.noreply.github.com>
2023-02-22 16:49:39 +0300

    new_update

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -4,41 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.service.CarServiceImpl;
+import web.models.Car;
+import web.service.CarService;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.util.List;
+
+import static web.service.CarService.index;
 
 
 @Controller
-public class CarsController extends CarServiceImpl {
+public class CarsController  {
 
     @GetMapping("/cars")
-    public String printCar(@RequestParam(value = "count", required = false) String count,
-                           @RequestParam(defaultValue = "5", required = false) String defaultValue, ModelMap model) {
-        System.out.println("Count = " + count + "  " + "defalutValue = " + defaultValue);
-        if (count == null) {
-            model.addAttribute("carList", index());
-            return "cars";
-        } else if (count.equals("1")) {
-            model.addAttribute("carList", Arrays.stream(index().toArray()).limit(1).collect(Collectors.toList()));
-            return "cars";
-        } else if (count.equals("2")) {
-            model.addAttribute("carList", Arrays.stream(index().toArray()).limit(2).collect(Collectors.toList()));
-            return "cars";
-        } else if (count.equals("3")) {
-            model.addAttribute("carList", Arrays.stream(index().toArray()).limit(3).collect(Collectors.toList()));
-            return "cars";
-        } else if (count.equals("4")) {
-            model.addAttribute("carList", Arrays.stream(index().toArray()).limit(4).collect(Collectors.toList()));
-            return "cars";
-        } else if (count.equals("5")) {
-            model.addAttribute("carList", Arrays.stream(index().toArray()).limit(5).collect(Collectors.toList()));
-            return "cars";
-        } else {
-            model.addAttribute("carList", index());
-            return "cars";
-        }
+    public String printCar(@RequestParam(value = "count", defaultValue = "5") int cars, ModelMap model) {
+      List<Car>carsList =  index();
+      carsList = CarService.carsCount(carsList,cars);
+     model.addAttribute("carList",carsList);
+     return "cars";
     }
+
 }
 
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,10 +1,47 @@
 package web.service;
 
+
 import web.models.Car;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public interface CarService {
-     List<Car> index();
+
+public class CarService {
+    private static int PEOPLE_COUNT;
+    private static  List<Car> carList;
+
+    static {
+        carList = new ArrayList<>();
+        carList.add(new Car("Spyder", "Ferrari", ++PEOPLE_COUNT));
+        carList.add(new Car("Quatroporte", "Maserati", ++PEOPLE_COUNT));
+        carList.add(new Car("Aventador", "Lamborghini", ++PEOPLE_COUNT));
+        carList.add(new Car("Italy", "Ferrari", ++PEOPLE_COUNT));
+        carList.add(new Car("Diablo", "Lamborghini", ++PEOPLE_COUNT));
+    }
+
+    @Override
+    public String toString() {
+        return "CarDaoImpl{" +
+                "carList=" + carList +
+                '}';
+    }
+
+
+    public static List<Car> index() {
+        return carList;
+    }
+
+
+
+    public  static List<Car> carsCount(List<Car> list, int number) {
+        if (number == 0 || number > 5) return list;
+        return list.stream().limit(number).collect(Collectors.toList());
+    }
+
+
 
 }
+
+
